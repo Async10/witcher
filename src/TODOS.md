26543034718ce211f8d6d10644904cf9117a051c
Daniel Schmitt <d.schmitt.privat@gmail.com>
2022-04-30 20:23:26 +0200

    "Erstelle Spiel"-Menü implementiert

--- src/TODOS.md
+++ src/TODOS.md
@@ -1,5 +1,4 @@
 # TODOS
 
-- Menü zur Auswahl eines neues Spiels
 - Bestenliste mit Filter nach Spiel
 - Icon für PWA
--- src/ui/Spiele/Spiele.tsx
+++ src/ui/Spiele/Spiele.tsx
@@ -1,5 +1,7 @@
 import AddOutlined from "@mui/icons-material/AddOutlined";
 import Fab from "@mui/material/Fab";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 import Stack from "@mui/material/Stack";
 import React from "react";
 import { useNavigate } from "react-router-dom";
@@ -22,8 +24,32 @@ export default function Spiele() {
   const [erstelleSpielDialogOpen, setErstelleSpielDialogOpen] =
     React.useState<boolean>(false);
 
-  const handleErstelleSpielClick = () => {
+  const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
+  const menuOpen = Boolean(anchorEl);
+
+  const handleErstelleSpielClick = (
+    ev: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    setAnchorEl(ev.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleUNOClick = () => {
+    setAnchorEl(null);
     setErstelleSpielDialogOpen(true);
+  };
+
+  const handleWizardClick = () => {
+    setAnchorEl(null);
+    alert("Sorry. Wizard kann noch nicht gespielt werden.");
+  };
+
+  const handleWitchesClick = () => {
+    setAnchorEl(null);
+    alert("Sorry. Witches kann noch nicht gespielt werden.");
   };
 
   const handleSpielStarten = ({ id }: ErstelltesSpiel) => {
@@ -43,12 +69,47 @@ export default function Spiele() {
       </Stack>
 
       <Fab
+        id="spiel-erstellen-menu-open-button"
         onClick={handleErstelleSpielClick}
-        aria-label="Neues Spiel beginnen"
+        aria-label={`"Spiel erstellen"-Menü öffnen`}
+        aria-controls={menuOpen ? "spiel-erstellen-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={menuOpen ? "true" : undefined}
         sx={{ position: "fixed", bottom: 0, right: 0, mb: 10, mr: 2 }}
       >
         <AddOutlined />
       </Fab>
+
+      <Menu
+        id="spiel-erstellen-menu"
+        anchorEl={anchorEl}
+        anchorOrigin={{ vertical: "top", horizontal: "left" }}
+        transformOrigin={{ vertical: "bottom", horizontal: "center" }}
+        open={menuOpen}
+        onClose={handleMenuClose}
+        MenuListProps={{
+          "aria-labelledby": "spiel-erstellen-menu-open-button",
+        }}
+      >
+        <MenuItem
+          arial-label="Neues UNO-Spiel starten"
+          onClick={handleUNOClick}
+        >
+          UNO
+        </MenuItem>
+        <MenuItem
+          aria-label="Neues Wizard-Spiel starten"
+          onClick={handleWizardClick}
+        >
+          Wizard
+        </MenuItem>
+        <MenuItem
+          aria-label="Neues Witches-Spiel starten"
+          onClick={handleWitchesClick}
+        >
+          Witches
+        </MenuItem>
+      </Menu>
 
       <ErstelleSpielDialog
         open={erstelleSpielDialogOpen}
